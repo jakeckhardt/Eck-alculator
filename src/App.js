be4c01f65e403e7cdaa94094b6ff5760f53c5c2b
Jake Eckhardt <jake.c.eckhardt@gmail.com>
2017-09-08 09:30:16 -0700

    Updated syntax to more modern React style.

--- src/App.js
+++ src/App.js
@@ -51,34 +51,33 @@ class App extends Component {
 
   addNum(e) {
     const val = e.target.value;
-    const classId = e.target.className;
     const lastChar = this.state.mathProb.slice(-1);
-    if (this.state.mathProb == "0" || this.state.mathProb == "error") {
-      if (val != "CE" && val != "=" && val != "*" && val != "/") {
+    if (this.state.mathProb === "0" || this.state.mathProb === "error") {
+      if (val !== "CE" && val !== "=" && val !== "*" && val !== "/") {
         this.setState({mathProb: this.state.mathProb = ""});
         this.setState({mathProb: this.state.mathProb += val});
-      } else if (val == "CE") {
+      } else if (val === "CE") {
         this.setState({mathProb: this.state.mathProb = "0"});
         this.setState({total: this.state.total = "0"});
       }
-    } else if (val == "CE") {
+    } else if (val === "CE") {
       this.setState({mathProb: this.state.mathProb = "0"});
       this.setState({total: this.state.total = "0"});
-    } else if (val == "=") {
-      if (lastChar == "+" || lastChar == "-" || lastChar == "*" || lastChar == "/"){
+    } else if (val === "=") {
+      if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/"){
         this.setState({mathProb: "error"});
         this.setState({total: this.state.total = "0"});
       } else {
         var totalNum = eval(this.state.mathProb).toString();
         this.setState({mathProb: this.state.total = totalNum});
       }
-    } else if (lastChar == "+" || lastChar == "-" || lastChar == "*" || lastChar == "/") {
-      if (val == "+" || val == "-" || val == "*" || val == "/") {
+    } else if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/") {
+      if (val === "+" || val === "-" || val === "*" || val === "/") {
         this.setState({mathProb: this.state.mathProb.slice(0,-1) + val});
       } else {
         this.setState({mathProb: this.state.mathProb += val});
       }
-    } else if (lastChar != "+" || lastChar != "-" || lastChar != "*" || lastChar != "/") {
+    } else if (lastChar !== "+" || lastChar !== "-" || lastChar !== "*" || lastChar !== "/") {
       this.setState({mathProb: this.state.mathProb += val});
     }
   }
